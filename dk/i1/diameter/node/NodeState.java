836ada91562a429dd066dc05b934f88f33dd600d
root <root@10075aad-742c-0410-a723-0128a6382dab>
2008-03-24 13:42:26 +0000

    Handle more than 2^31 sessions connectly when generating Session-IDs (integer overflow->negative)

--- dk/i1/diameter/node/NodeState.java
+++ dk/i1/diameter/node/NodeState.java
@@ -5,7 +5,7 @@ class NodeState {
 	private int state_id;
 	private int end_to_end_identifier;
 	private int session_id_high;
-	private int session_id_low;
+	private long session_id_low;
 	
 	NodeState() {
 		int now = (int)(System.currentTimeMillis()/1000);
@@ -26,11 +26,13 @@ class NodeState {
 	}
 	
 	synchronized String nextSessionId_second_part() {
-		int l = session_id_low;
+		long l = session_id_low;
 		int h = session_id_high;
 		session_id_low++;
-		if(session_id_low==0)
+		if(session_id_low==4294967296L) {
+			session_id_low=0;
 			session_id_high++;
+		}
 		return h + ";" + l;
 	}
 }
